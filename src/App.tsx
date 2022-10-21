2d6b2e92026a6de2b8afc5b6ab3115022aa50c29
Pepijn <pvwpvw@protonmail.com>
2022-10-21 19:10:03 +0200

    First commit, rough starting framework worked out

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,16 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import LineChart from './components/LineChart';
+import Stats from './components/Stats';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return (
+		<div className="bg-slate-800 h-screen p-10 flex justify-between">
+			<LineChart></LineChart>
+			<Stats></Stats>
+		</div>
+	);
 }
 
 export default App;
---
+++ src/components/LineChart.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function LineChart() {
+	return (
+		<div className="border border-stone-50 w-4/6 h-5/6 flex-initial">
+            <p className='text-white text-center'>LINE CHART GEWICHT ENZO</p>
+		</div>
+	);
+}
+
+export default LineChart;
